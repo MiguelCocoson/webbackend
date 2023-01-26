a40c318c6e02eb9ee2cad503e1a8800ec5167a70
Miguel Marin <miguelmarinjavier5@gmail.com>
2023-01-25 22:09:34 -0700

    permission

--- app.js
+++ app.js
@@ -7,8 +7,16 @@ const app = express();
 
 app
   .use(bodyParser.json())
-  .use((req, res, next) => {
+  app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+    );
     next();
   })
   .use('/', require('./routes'));
